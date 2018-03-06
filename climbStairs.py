5eda8c15a4e848f43fad55a48a6cdea38e74a1f5
simple1_6 <454340056@qq.com>
2018-03-06 12:16:40 +0800

    解决注释乱码

--- climbStairs.py
+++ climbStairs.py
@@ -1,11 +1,11 @@
 def climbStairs(n):
     frequency=[1,1]
-    #计数从0开始
+    '''计数从0开始'''
     for i in range(2,n+1):
         frequency.append(frequency[i-1]+frequency[i-2])
     return frequency[n]
 
-#测试：
+'''测试：'''
 if __name__=='__main__':
     assert climbStairs(3)==3,'Calculation error'
     assert climbStairs(5)==8, 'Calculation error'
