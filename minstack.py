8386090e35e946d2b6f5b63e0bb9cce1495677f8
simple1_6 <454340056@qq.com>
2018-04-10 11:09:10 +0800

    *

--- minstack.py
+++ minstack.py
@@ -44,44 +44,5 @@ class MinStack:
 
         print('min:self.zhan', self.zhan)
         return min(self.zhan)
-    #
 
-a=MinStack()
-print(a.push(1),'/n',a.pop(),'/n',a.push(2),'/n',a.push(3),'/n',a.min(),'/n',a.push(1),'pop',a.pop(),a.pop(),a.pop(),'/n',a.min())
-# a.push(1)
-# print(a.pop())
-# a.push(2)
-# a.push(3)
-# a.min()
-# a.push(1)
-# print(a.min())
 
-# def catch_exception(origin_func):
-#     def wrapper(self, *args, **kwargs):
-#         try:
-#             u = origin_func(self, *args, **kwargs)
-#             return u
-#         except Exception:
-#             print('here3')
-#
-#             self.revive() #不用顾虑，直接调用原来的类的方法
-#             return 'an Exception raised.'
-#     return wrapper
-#
-#
-# class Test(object):
-#     def __init__(self):
-#         pass
-#
-#     def revive(self):
-#         print('revive from exception.')
-#         # do something to restore
-#
-#     @catch_exception
-#     def read_value(self):
-#         print('here I will do something.')
-#         # do something.
-#
-# a=Test()
-# a.revive()
-# a.read_value()
